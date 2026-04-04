e0cae121395cfcdb9cee591c3284514e5caa882b
rcam3937 <jcam3937@gmail.com>
2026-04-04 13:29:06 -0500

    please work

---
+++ redVSblue.py
@@ -0,0 +1,55 @@
+import requests
+import csv
+
+
+BASE_URL = "https://www.thebluealliance.com/api/v3"
+API_KEY = "F5CQwbGuceE3Zc5d97Gm0VEuzdXoLqrbkYbThdSd3HlaKEEWZ2y7wejsnBzgVCGY"
+team = "frc3937"
+
+HEADERS = {
+    "X-TBA-Auth-Key": API_KEY
+}
+
+def get_years_active(team_key):
+    url = f"{BASE_URL}/team/{team_key}/years_participated"
+    response = requests.get(url, headers=HEADERS)
+
+    return response.json()
+
+def get_matches_from_year(team_key, year):
+    url = f"{BASE_URL}/team/{team_key}/matches/{year}/simple"
+    response = requests.get(url, headers=HEADERS)
+
+    return response.json()
+
+years = get_years_active(team)
+
+for year in years:
+    redWins = 0
+    blueWins = 0
+    redLosses = 0
+    blueLosses = 0
+    blueTies = 0
+    redTies = 0
+
+    matches = get_matches_from_year(team, year)
+    for match in matches:
+        if team in match["alliances"]["red"]["team_keys"]:
+            if match["winning_alliance"] == "red":
+                redWins += 1
+            elif match["winning_alliance"] == "blue":
+                redLosses += 1
+            else:
+                redTies += 1
+        elif team in match["alliances"]["blue"]["team_keys"]:
+            if match["winning_alliance"] == "blue":
+                blueWins += 1
+            elif match["winning_alliance"] == "red":
+                blueLosses += 1
+            else:
+                blueTies += 1
+    print(f"{year}: Red Wins: {redWins}, Red Losses: {redLosses}, Red Ties: {redTies}, Blue Wins: {blueWins}, Blue Losses: {blueLosses}, Blue Ties: {blueTies}")
+    #Write to csv
+    with open('red_vs_blue.csv', mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([year, redWins, redLosses, redTies, blueWins, blueLosses, blueTies])
